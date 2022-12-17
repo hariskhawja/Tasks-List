2e0c6071cee89e75e59acc91c57da6e7123a82d1
Haris Khawja <92524990+hariskhawja@users.noreply.github.com>
2022-12-17 11:59:39 -0500

    commit: final

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 var all_sections = {}
 var section_id_dict = {}
 var section_counter = 1
+var num_tasks = 0
 
 
 function addTask(task_id, task_section_id, time_id, section1_id, section2_id, section3_id, section4_id){
     const task = document.getElementById(task_id).value
-    const task_section = document.getElementById(task_section_id).value
+    const section = document.getElementById(task_section_id).value.split(' ')
     const time = document.getElementById(time_id).value
+    var task_section = ""
+    for (let i = 0; i < section.length; i++){
+        task_section += section[i]
+    }
     if (task_section in all_sections){
         all_sections[task_section].push([task, time])
+        num_tasks ++
+        document.getElementById('numTasks').innerHTML = num_tasks
     }
 
     else {
         if(Object.keys(all_sections).length < 4){
             all_sections[task_section] = [[task, time]]
-            section_id_dict[task_section] = "section" + section_counter
+            section_id_dict[task_section] = "Section" + section_counter
             section_counter ++ 
 
             var count = 0
@@ -32,6 +39,8 @@ function addTask(task_id, task_section_id, time_id, section1_id, section2_id, se
                     document.getElementById(section4_id).innerHTML = section
                 }
                 count ++ 
+                num_tasks ++
+                document.getElementById('numTasks').innerHTML = num_tasks
             }
         }
         else {
@@ -40,6 +49,7 @@ function addTask(task_id, task_section_id, time_id, section1_id, section2_id, se
     }
 
     if (task_section in section_id_dict){
+      console.log(task_section)
       openSection(section_id_dict[task_section], "output_box")
     }
     console.log(all_sections)
@@ -70,8 +80,16 @@ function minusTime(section_id, task_id){
     if (section in all_sections){
         for (let i = 0; i < all_sections[section].length; i++){
             if (all_sections[section][i][0] == task){
+              if (all_sections[section][i][1] > 0){
                 all_sections[section][i][1] = Number(all_sections[section][i][1]) - 1
-                console.log(all_sections[section][i][1])
+              }
+
+              if (all_sections[section][i][1] <= 0){
+                all_sections[section].splice(i, 1)
+                num_tasks -= 1
+                document.getElementById('numTasks').innerHTML = num_tasks
+              }
+
                 openSection(section_id_dict[section], "output_box")
                 return true
     
@@ -94,3 +112,38 @@ function openSection(section_id, text_id){
     console.log(val)
     document.getElementById(text_id).innerHTML = val
 }
+
+function deleteSection(){
+    const curr_section = document.getElementById("output_box").innerHTML.split(" ")[0]
+    console.log(curr_section)
+    if (curr_section in all_sections && curr_section in section_id_dict){
+        num_tasks -= all_sections[curr_section].length
+        document.getElementById('numTasks').innerHTML = num_tasks
+        all_sections[curr_section] = []
+        openSection(section_id_dict[curr_section], "output_box")
+    }
+    else{
+        return false
+    }
+  }
+
+function renameSection(input_id){
+      const name = document.getElementById(input_id).value.split(" ")
+      const curr_section = document.getElementById("output_box").innerHTML.split(" ")[0]
+      var new_name = ""
+      for (let i = 0; i < name.length; i++){
+          new_name += name[i]
+      }
+      console.log(new_name)
+      if (curr_section){
+          document.getElementById(section_id_dict[curr_section]).innerHTML = new_name
+          const temp = all_sections[curr_section]
+          const temp2 = section_id_dict[curr_section]
+          delete all_sections[curr_section]
+          delete section_id_dict[curr_section]
+          all_sections[new_name] = temp
+          section_id_dict[new_name] = temp2
+          openSection(section_id_dict[new_name], "output_box")
+      }
+}
+  
